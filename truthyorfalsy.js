ec222f3788a1848af98ae716b9a9e013ae581d40
Jassy-J <65877445+Jassy-J@users.noreply.github.com>
2020-06-16 16:04:36 +0900

    Create truthyorfalsy.js

---
+++ truthyorfalsy.js
@@ -0,0 +1,13 @@
+const truthyOrFalsy=(value)=> {
+    if (value) {
+       return true;
+    } else {
+      return false ;
+    }
+}
+
+console.log(truthyOrFalsy('12')) 
+//print true
+
+console.log(truthyOrFalsy(0)) 
+//print false
